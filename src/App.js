d8e86273d80e0b9501754bc912fb074ac42c961d
Priyanshu Kunwar <priyanshukunwar7@gmail.com>
2023-07-30 12:54:48 +0530

    replace to back

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter basename="/YouTube">
+    <BrowserRouter>
       <Box sx={{ backgroundColor: "#000" }}>
         <Navbar />
         <Routes>
-          <Route path="/YouTube" exact element={<Feed />} />
+          <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetails />} />
           <Route path="/channel/:id" element={<ChannelDetails />} />
           <Route path="/search/:searchTerm" element={<SearchFeed />} />
